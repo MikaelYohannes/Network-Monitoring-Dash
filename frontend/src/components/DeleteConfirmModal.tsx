8a915a999cb0f4de45197aa2860fd04c80cadf97
Michael Yohannes <tesfamikaelsolomon@gmail.com>
2026-07-21 21:58:05 -0600

    Delete device clean up + install routing

--- frontend/src/components/DeleteConfirmModal.tsx
+++ frontend/src/components/DeleteConfirmModal.tsx
@@ -10,10 +10,17 @@ export default function DeleteConfirmModal({
   if (!isOpen) return null;
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <h2 className="text-xl">Delete Device?</h2>
+      <div className="flex justify-cernter flex-col p-10 border border-orange-400 rounded-xl">
+        <h2 className="text-xl mb-5">Delete Device?</h2>
 
-      <p>Are you sure you want to delete {deviceName}?</p>
-      <button onClick={onConfirm}>Delete</button>
+        <p>Are you sure you want to delete {deviceName}?</p>
+        <button
+          onClick={onConfirm}
+          className="mt-7 border border-red-500 w-50 text-red-500 rounded-xl hover:cursor-pointer hover:bg-red-500 hover:text-white duration-500"
+        >
+          Delete
+        </button>
+      </div>
     </Modal>
   );
 }
